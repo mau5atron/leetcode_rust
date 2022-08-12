1f06ea2dcac85b3858d01709ab2657426ce2a2f9
mau5atron <gabrielbetancourt96@outlook.com>
2022-08-12 16:27:50 -0400

    Added solution for maximum product of sub array

---
+++ medium/maximum_product_subarray/src/main.rs
@@ -0,0 +1,68 @@
+
+struct Solution {}
+
+impl Solution {
+    // pub fn max_product(nums: Vec<i32>) -> i32 {
+    //     let mut current_product: i32 = 0;
+
+    //     for (idx, num) in nums.iter().enumerate(){
+    //         if num < &1{
+    //             continue;
+    //         }
+
+    //         if (idx+1) < nums.len() && nums[idx+1].is_positive(){
+    //             current_product = num*(nums[idx + 1]);
+    //         }
+    //         println!("current num: {}", num);
+    //     }
+    //     println!("Current product : {}", current_product);
+    //     return current_product;
+    // }
+
+
+//     pub fn max_product(nums: Vec<i32>) -> i32 {
+//         if nums.len() < 2 {
+//             return nums[0];
+//         }
+        
+//         let mut current_product: i32 = 0;
+
+//         for (idx, num) in nums.iter().enumerate(){
+
+//             if (idx+1) < nums.len(){
+//                 let product = num*(nums[idx + 1]);
+//                 // println!("product : {}", product);
+//                 if product.is_positive() && current_product < 1{
+//                     current_product = product;
+//                 }
+//             }
+            
+//             // if (idx+1) < nums.len() && nums[idx+1].is_negative(){
+//             //     current_product = 
+//             // }
+//             // println!("current num: {}", num);
+//         }
+//         // println!("Current product : {}\n", current_product);
+//         return current_product;
+//     }
+    pub fn max_product(nums: Vec<i32>) -> i32 {
+        let mut min_product = 1;
+        let mut max_product = 1;
+        let mut result = nums[0];
+
+        for (idx, num) in nums.iter().enumerate(){
+            let temp_max_product = max_product * nums[idx];
+            max_product = std::cmp::max(nums[idx], std::cmp::max(nums[idx]*max_product, nums[idx]*min_product));
+            min_product = std::cmp::min(nums[idx], std::cmp::min(temp_max_product, nums[idx]*min_product));
+
+            result = std::cmp::max(result, std::cmp::max(max_product, min_product));
+        }
+
+        return result;
+    }
+}
+
+fn main() {
+    Solution::max_product(Vec::from(vec![-3, 3, 4, -2, 4]));
+    Solution::max_product(Vec::from(vec![-3, -1, -1]));
+}
