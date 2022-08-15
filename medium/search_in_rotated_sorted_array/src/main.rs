9de3c3457ad90624fc809e0047a9bb31bfa60425
mau5atron <gabrielbetancourt96@outlook.com>
2022-08-15 00:58:17 -0400

    Added solution, notes, and test for search in rotated sorted array

--- medium/search_in_rotated_sorted_array/src/main.rs
+++ medium/search_in_rotated_sorted_array/src/main.rs
@@ -19,8 +19,8 @@
 // Example 2:
 // Input: nums = [4,5,6,7,0,1,2], target = 3
 // Output: -1
-// Example 3:
 
+// Example 3:
 // Input: nums = [1], target = 0
 // Output: -1
  
@@ -37,10 +37,61 @@ struct Sol {}
 
 impl Sol {
     pub fn search(nums: Vec<i32>, target: i32) -> i32 {
-        return 1;
+        // init left and right side of array index
+        let mut left = 0;
+        let mut right = nums.len() - 1;
+
+        while left <= right {
+            let mut mid = (left + right) / 2;
+            if nums[mid] == target {
+                return mid as i32;
+            }
+
+            // check if left section is sorted
+            if nums[left] <= nums[mid]{
+                // if this is true, means numbers from left to mid are already
+                // in ascending order
+                if (target < nums[left]) || (target > nums[mid]){
+                    // move left pointer to mid + 1
+                    left = mid + 1;
+                } else {
+                    // move right pointer from right to mid - 1
+                    right = mid - 1;
+                }
+            } else {
+                if (target < nums[mid]) || (target > nums[right]){
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+        }
+
+        return -1; // if num does not exist, return -1
     }
 }
 
 fn main() {
-    // Sol::search(nums, target)
+}
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test1(){
+        let result: i32 = 4;
+        assert_eq!(result, super::Sol::search(Vec::from(vec![4,5,6,7,0,1,2]), 0));
+    }
+
+    #[test]
+    fn test2(){
+        let result = -1;
+        assert_eq!(result, super::Sol::search(Vec::from(vec![4,5,6,7,0,1,2]), 3))
+    }
+
+    #[test]
+    fn test3(){
+        let result = -1;
+        assert_eq!(result, super::Sol::search(Vec::from(vec![1]), 0));
+    }
 }
